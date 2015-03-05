0fbaeadab82d71e7ff399fa45e52d2a2262cf046
Erik <erik@Newmac-2.local>
2015-03-05 13:33:06 -0800

    Initial Commit

---
+++ aCDSprototype1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  aCDSprototype1
+//
+//  Created by Erik on 3/5/15.
+//  Copyright (c) 2015 FWS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ aCDSprototype1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  aCDSprototype1
+//
+//  Created by Erik on 3/5/15.
+//  Copyright (c) 2015 FWS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
